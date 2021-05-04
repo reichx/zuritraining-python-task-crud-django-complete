e8a8e238d3e7878b50946a069eddf3f51cc5aaf1
reichx <66280338+reichx@users.noreply.github.com>
2021-05-04 22:36:45 +0100

    Task on CRUD Operations in Django - Extend Blog App By Adding New Features (Task Complete)

---
+++ blog/views.py
@@ -0,0 +1,44 @@
+# from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Post
+
+# Create your views here.
+
+class BlogListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'post_list.html'
+
+class BlogDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'body']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
