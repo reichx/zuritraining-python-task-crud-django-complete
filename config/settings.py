1a1d71f027eacb2861d7be28db9f66fdafc30452
reichx <66280338+reichx@users.noreply.github.com>
2021-05-05 00:03:36 +0100

    Task on CRUD Operations in Django - Implemented Email SMTP API for Password Reset Feature & some minor changes

--- config/settings.py
+++ config/settings.py
@@ -145,4 +145,10 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'info@isourc.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
